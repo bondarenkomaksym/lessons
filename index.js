1c2ed6320716f925b2b1308c80da54a8a8747938
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-08 12:22:24 +0300

    master

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+/*
+
+const userName = 'John';
+const greeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDESSERT = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
+
+*/
