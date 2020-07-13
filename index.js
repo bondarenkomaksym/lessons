0514fa34b608b1e1398cd75f17c6e6dc35f32c93
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-13 18:02:44 +0300

    master

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let a = 1;
-let n = 187;
-let sum = 0;
-while (a <= n) {
-  sum += a;
-  a++;
-}
-console.log("Result: " + sum);
+let m = 2;
+let n = 8;
+let result = 1;
+do {
+  if (m % 2 === 1) {
+    result *= m;
+  }
+  m++;
+} while (m <= n);
+console.log("Result:" + result);
