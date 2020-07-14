32ec1fcee37e8075cad8b7ffb0a75eb1a70930c5
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-14 15:16:03 +0300

    master

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let m = 7;
 let n = 12;
 let i = m;
 let j = n;
-let result = "";
+let result = 0;
 for (let i = 7; i <= j; i++) {
   if ((i % 2 === 0, i % 4 === 1)) {
     result += i;
@@ -11,7 +11,7 @@ for (let i = 7; i <= j; i++) {
   } else if (i % 4 === 0) {
     result *= i;
   } else if (i % 5 === 0) {
-    console.log(result);
+    console.log(i);
   } else {
     break;
   }
