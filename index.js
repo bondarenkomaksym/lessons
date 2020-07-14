09c322a7af2d65339e3a681ee24e8de32506b14f
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-14 17:39:30 +0300

    master

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let sum = 0;
-for (let i = 0; i <= 1000; i++) {
-  if (i % 2 === 1) {
-    console.log("Found");
-    sum += i;
-  }
+let a = 0;
+let b = 10000000;
+while (a <= b) {
+  sum += a;
+  a++;
 }
-if (sum * 5 > 5000) {
-  console.log("Bigger");
-} else {
-  console.log("Smaller or equal");
-}
+console.log(sum);
