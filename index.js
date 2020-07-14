ad80743673181591c78c6531385bf1e57a5312fb
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-14 15:09:36 +0300

    master

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let n = 3;
-let i = n;
-let sum = "";
-for (let i = 1; i <= n; i++) {
-  sum += i;
+let m = 7;
+let n = 12;
+let i = m;
+let j = n;
+let result = 0;
+for (let i = 7; i <= j; i++) {
+  if ((i % 2 === 0, i % 4 === 1)) {
+    result += i;
+  } else if (i % 3 === 0) {
+    result -= i;
+  } else if (i % 4 === 0) {
+    result *= i;
+  } else if (i % 5 === 0) {
+    console.log(result);
+  } else {
+    break;
+  }
 }
-console.log(sum);
