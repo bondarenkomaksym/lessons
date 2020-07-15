f05f0d8c9f3af195aac2555ec614eccede665448
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-15 15:07:29 +0300

    master

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ while (a <= b) {
   sum += a;
   a++;
 }
-let small = (sum / 1234).toFixed();
+let small = (sum / 1234);
 let big = (sum % 1234);
 (small > big) ? console.log("true") : console.log("false");
-
