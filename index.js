882f0a7b1453035c4d578518fb3903dc54adab92
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-16 18:17:23 +0300

    master

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ function increaser(a, index) {
   for (let i = a; i > index; i += index) {
     return a + index;
   }
-  for (let i = a; i <= index; i += index) {
-    return a + index;
-  }
 }
 console.log(increaser(11, 8));
