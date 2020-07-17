ae6e645d2ed4b782ccc2c083f939dd00ad1e2b23
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-17 14:31:07 +0300

    master

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function createEmptyArray(len) {
-  let empty = new Array(len);
-  return empty;
+const numbers = [2, 3, 4, 5];
+
+function squareArray(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  let squaredA = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    squaredA.push(Math.pow(arr[i], 2));
+  }
+
+  return squaredA;
 }
+
+const array = squareArray(numbers);
+console.log(array);
