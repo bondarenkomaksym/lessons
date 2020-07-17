7908312ffc3c176b02ddaaf3031cf2897bd18114
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-17 14:49:17 +0300

    master

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const numbers = [2, 3, 4, 5];
 
-function squareArray(arr) {
+function chekSum(arr) {
   if (!Array.isArray(arr)) {
     return null;
   }
+
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
@@ -11,5 +12,5 @@ function squareArray(arr) {
   return sum;
 }
 
-const array = squareArray(numbers);
-console.log(array);
+const array = chekSum(numbers);
+console.log(array > 100 ? true : false);
