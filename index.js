ca597b3dd7216e7b01adf60970c51243375693ca
bondarenkomaksym <maksimphotographer@gmail.com>
2020-07-17 16:03:00 +0300

    master

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-// const numbers = ([2, 5, 6, 8, 11, 9, 4], 20);
+const numbers = [2, 5, 6, 8, 11, 9, 4];
 
-function increaseEvenEl(arr, delta) {
+function reverseArray(arr) {
   if (!Array.isArray(arr)) {
     return null;
   }
 
-  let out = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      out.push(arr[i] + delta);
-    }
-  }
-  return out;
+  return arr.reverse();
 }
 
-const array = increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20);
+const array = reverseArray(numbers);
 console.log(array);
